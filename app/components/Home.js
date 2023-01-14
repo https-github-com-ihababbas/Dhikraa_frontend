b81d1b7287d7a49dc8fd48b3dd46c59d14811672
alghzzawi <mohammadalghazzawi9@gmail.com>
2023-01-14 22:10:41 +0200

    feat : add css file and edit home file

--- app/components/Home.js
+++ app/components/Home.js
@@ -3,8 +3,7 @@
 import React from "react";
 import { useState } from "react";
 import axios from "axios";
-import Image from "next/image";
-import home_img from 'public/assets/home_dhekraa.png'
+
 
 
 
@@ -12,11 +11,11 @@ export default function Home() {
 
     const [prayTimeState, SetPrayTimeState] = useState({})
     const [prayaddressState, SetPrayaddressState] = useState({})
+
     const date = new Date();
     let year = date.getFullYear();
     let month = date.getMonth() + 1;
     let day = date.getDate()
-
 
     async function prayTimes() {
         'this function give an opject of pray times from api'
@@ -52,54 +51,42 @@ export default function Home() {
             }
         })
     }
-    // console.log(prayTimeState)
 
     return (
-        <section className="bg-white">
-            <section className="mr-auto place-self-center lg:col-span-7">
-                <Image
-                    className="hidden lg:block relative h-[400px]"
-                    src={home_img}
-                />
-                <Image
-                    className="block lg:hidden relative h-[300px]"
-                    src={home_img}
-                />
-            </section>
+        <section on={prayTimes()} className="bg-white place-content-center">
 
-
-            <section on={prayTimes()} className="flex md:container md:mx-auto scroll-my-1.5">
+            <section className="flex md:container md:mx-auto scroll-my-1.5">
                 <table className="table-auto bg-white w-[60%] text-center text-black">
-                    <thead>
 
-                        <th>
-                            <tr> الصلاة</tr>
-                        </th>
-                        <th>
-                            <tr>وقت الأذان</tr>
-                        </th>
-                    </thead>
-                    <tbody>
-                        <th>
-                            <tr>الفجر</tr>
-                            <tr>الشروق</tr>
-                            <tr>الظهر</tr>
-                            <tr>العصر</tr>
-                            <tr>المغرب</tr>
-                            <tr>العشاء</tr>
+                    <tr>
+                        <th>الصلاة</th>
+                        <th>وقت الأذان</th>
+                    </tr>
+                    <tr>
+                        <td>الفجر</td>
+                        <td>{prayTimeState.Fajr}</td>
+                    </tr>
+                    <tr>
+                        <td>الشروق</td>
+                        <td>{prayTimeState.Sunrise}</td>
+                    </tr>
+                    <tr>
+                        <td>الظهر</td>
+                        <td>{prayTimeState.Dhuhr}</td>
+                    </tr>
+                    <tr>
+                        <td>العصر</td>
+                        <td>{prayTimeState.Asr}</td>
+                    </tr>
+                    <tr>
+                        <td>المغرب</td>
+                        <td>{prayTimeState.Maghrib}</td>
+                    </tr>
+                    <tr>
+                        <td>العشاء</td>
+                        <td>{prayTimeState.Isha}</td>
+                    </tr>
 
-                        </th>
-                        <th>
-                            
-
-                            <tr>{prayTimeState.Fajr}</tr>
-                            <tr>{prayTimeState.Sunrise}</tr>
-                            <tr>{prayTimeState.Dhuhr}</tr>
-                            <tr>{prayTimeState.Asr}</tr>
-                            <tr>{prayTimeState.Maghrib}</tr>
-                            <tr>{prayTimeState.Isha}</tr>
-                        </th>
-                    </tbody>
                 </table>
                 <section className="text-black text-right">
                     <h1>التاريخ : {prayaddressState.gregorian}</h1>
