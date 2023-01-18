48da933950ab5611e8c2350695a556197acf2f2f
WalaaAtiah <walaaatiah97@gmail.com>
2023-01-18 07:45:51 +0000

    feat:add profile page

--- app/contexts/auth.js
+++ app/contexts/auth.js
@@ -10,18 +10,34 @@ export default function AuthWrapper({children}){
         tokens: localStorage.getItem("tokens"),
         login,
         logout,
+        
     })
 
     async function login (userInfo) {
-        const url = "https://cookie-w-a.herokuapp.com/api/token/";
+        const url = "https://dhiker-api-v1.herokuapp.com/api/token/";
+        try{
+
+            const res = await axios.post(url, userInfo);
+            console.log(2,res.data)
+            SetGlobalState({
+                tokens : res.data,
+                login,
+            })
+        }catch {
+            console.log("error")
+        }
+        
         const res = await axios.post(url, userInfo);
-        console.log(res.data)
         setGlobalState({
             tokens: res.data,
             login,
             logout,
+            
         })
+        console.log("uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu",res.data)
         localStorage.setItem("tokens", res.data);
+        localStorage.setItem("access", res.data.access);
+        localStorage.setItem("username", userInfo.username);
     }
 
     async function logout() {
@@ -29,9 +45,12 @@ export default function AuthWrapper({children}){
             tokens: null,
             login,
             logout,
+           
         })
     
         localStorage.removeItem("tokens");
+        localStorage.removeItem("access");
+        localStorage.removeItem("username");
       }
 
 
