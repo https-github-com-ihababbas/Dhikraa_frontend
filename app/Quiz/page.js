b2699bc24be88801e30e1fa95f341c343a9bd262
WalaaAtiah <walaaatiah97@gmail.com>
2023-01-20 20:38:03 +0000

    feat:quiz page with ihab and amani

--- app/Quiz/page.js
+++ app/Quiz/page.js
@@ -6,15 +6,18 @@ import { AuthContext } from "../contexts/auth";
 import useSWR from 'swr'
 import axios from "axios";
 import Answer from "../components/answer";
+import Review from "./Review"
 
 export default function Todo() {
     const [strtflag, setStrtFlag] = useState(false)
+    const [submittflag, setSubmittFlag] = useState(false)
+    const [reviewflag, setReviwFlag] = useState({})
     const x = localStorage.getItem("username")
     const { tokens, refresh } = useContext(AuthContext)
     const refresh_string = localStorage.getItem("refresh")
     refresh(refresh_string)
     const [mark, setMark] = useState(0);
-    
+
     //Data Fetching part
     const access = localStorage.getItem("access")
 
@@ -52,34 +55,42 @@ export default function Todo() {
     function startQuiz() {
         setStrtFlag(true)
         setMark(0)
+        setSubmittFlag(false)
     }
 
-   
-    
+
+
     const submittHandeler = () => {
         // e.preventDefault();
 
 
         console.log("Submitt")
-        console.log(Object.values(answer).reduce((a, b) => a + b))
-        setMark(Object.values(answer).reduce((a, b) => a + b))
+        if (Object.values(answer).length != 0) {
+
+            console.log(Object.values(answer).reduce((a, b) => a + b))
+            setMark(Object.values(answer).reduce((a, b) => a + b))
+        }
         setStrtFlag(false)
-        
-        
-       
+        setSubmittFlag(true)
+        console.log(review)
+        setReviwFlag(review)
+
+
+
 
     }
     let answer = {}
+    let review = {}
 
     const answeruser = (value, id) => {
         console.log(id, value)
-        if( value ==number_to_view[id].choices.correct){
+        if (value == number_to_view[id].choices.correct) {
             answer[id] = 1
-        }else{
-            
+        } else {
+
             answer[id] = 0
         }
-        
+        review[id] = value
         console.log(answer)
         // localStorage.setItem(id, value);
 
@@ -100,10 +111,20 @@ export default function Todo() {
                     <div
 
                         className="pt-12 pb-12 font-sans text-gray-700 w-[40%] mr-10"
+
                     >
+                        {submittflag &&<button onClick={startQuiz} type="button" className="mr-14 py-1.5 px-2 flex text-white bg-gradient-to-r from-[#1e8a9d] to-[#187584] hover:bg-gradient-to-l focus:outline-none focus:ring-purple-200 font-medium rounded-2xl text-sm  text-center  ">
+
+                            <div className="text-xl font-mono font-bold pt-0.5 px-2">
+                                اعادة الاختبار
+                            </div>
+                            <span className="px-2 py-4 font-mono text-2xl text-gray-100 border-l-2 border-gray-100">
+                            </span>
+                        </button>}
 
 
-                        {!strtflag && <button onClick={startQuiz} type="button" className="mr-14 py-1.5 px-2 flex text-white bg-gradient-to-r from-[#1e8a9d] to-[#187584] hover:bg-gradient-to-l focus:outline-none focus:ring-purple-200 font-medium rounded-2xl text-sm  text-center  ">
+
+                        {!strtflag && !submittflag && <button onClick={startQuiz} type="button" className="mr-14 py-1.5 px-2 flex text-white bg-gradient-to-r from-[#1e8a9d] to-[#187584] hover:bg-gradient-to-l focus:outline-none focus:ring-purple-200 font-medium rounded-2xl text-sm  text-center  ">
 
                             <div className="text-xl font-mono font-bold pt-0.5 px-2">
                                 بدأ الاختبار
@@ -111,41 +132,55 @@ export default function Todo() {
                             <span className="px-2 py-4 font-mono text-2xl text-gray-100 border-l-2 border-gray-100">
                             </span>
                         </button>}
-                       
 
 
 
-                            {strtflag && number_to_view.map((item, i) => (
+
+                        {strtflag && number_to_view.map((item, i) => (
 
 
 
-                                <div className="flex-1 mt-1 text-right">
-                                    <div
+                            <div className="flex-1 mt-1 text-right">
+                                <div
 
-                                        className="w-full px-8 py-4 bg-white border rounded-lg shadow-lg">
+                                    className="w-full px-8 py-4 bg-white border rounded-lg shadow-lg">
 
-                                        <h2 className="font-bold leading-none text-gray-900 ">
-                                            <span className="block pb-2 text-sm text-blue-700"> {i + 1} السؤال</span>
-                                            <span className="block pb-2">{item.question}</span>
-                                        </h2>
+                                    <h2 className="font-bold leading-none text-gray-900 ">
+                                        <span className="block pb-2 text-sm text-blue-700"> {i + 1} السؤال</span>
+                                        <span className="block pb-2">{item.question}</span>
+                                    </h2>
 
-                                        <Answer
-                                            choices={item.choices}
-                                            id={i}
-                                            
-                                            answeruser={answeruser}
-                                        />
-                                    </div>
+                                    <Answer
+                                        choices={item.choices}
+                                        id={i}
 
+                                        answeruser={answeruser}
+                                    />
                                 </div>
 
+                            </div>
 
 
 
-                            ))}
-                            <button onClick={submittHandeler} >submit</button>
-                            <h1>{mark}</h1>
-                       
+
+                        ))}
+                        {!submittflag && strtflag && <button onClick={submittHandeler} >submit</button>}
+
+                        {submittflag &&
+                            <>
+
+                                <h1>{mark}/10</h1>
+                                <Review
+                                    number_to_view={number_to_view}
+                                    startQuiz={startQuiz}
+
+
+                                    review={review}
+                                />
+                            </>
+
+                        }
+
 
 
 
--- app/components/answer.js
+++ app/components/answer.js
@@ -5,7 +5,7 @@ import React from "react";
 
 
 
-export default function answer(props){     
+export default function answer(props) {
     let value = 0
     let answers = []
     value = Object.values(props.choices)
@@ -17,42 +17,43 @@ export default function answer(props){
         answers.push(props.choices.correct)
     }
     //console.log(answers)
-    
-    
-    const saveAnswers = (id,e) => {
-        props.answeruser(e.target.value,id)
-    };  
-    
-    
-    
 
-      
 
-    
-    
-    
-    return(
+    const saveAnswers = (id, e) => {
+        props.answeruser(e.target.value, id)
+    };
+
+
+
+
+
+
+
+
+
+    return (
 
         <>
-         
-           <p>
-
-           <section clasName="w-[30%]">
-
-            {value[0]} <input type="radio" id="answer"  name={props.id} value={value[0]} className="text-left" onChange={(e)=>{saveAnswers(props.id,e)}}/>
-            </section>
-            <section clasName="w-[30%]">
-            {value[1]} <input type="radio" id="answer"  name={props.id} value={value[1]} className="text-left" onChange={(e)=>{ saveAnswers(props.id,e)}}/>
-            </section>  
-            <section clasName="w-[30%]">
-            {value[2]} <input type="radio" id="answer"  name={props.id} value={value[2]} className="text-left" onChange={(e)=>{saveAnswers(props.id,e)}}/>
-            </section>  
-            <section clasName="w-[30%]">
-            {value[3]} <input type="radio" id="answer"  name={props.id} value={value[3]} className="text-left" onChange={(e)=>{saveAnswers(props.id,e)}} />
-            </section>
             
+
+            <p>
+
+                <section clasName="w-[30%]">
+
+                    {value[0]} <input type="radio" id="answer" name={props.id} value={value[0]} className="text-left" onChange={(e) => { saveAnswers(props.id, e) }} />
+                </section>
+                <section clasName="w-[30%]">
+                    {value[1]} <input type="radio" id="answer" name={props.id} value={value[1]} className="text-left" onChange={(e) => { saveAnswers(props.id, e) }} />
+                </section>
+                <section clasName="w-[30%]">
+                    {value[2]} <input type="radio" id="answer" name={props.id} value={value[2]} className="text-left" onChange={(e) => { saveAnswers(props.id, e) }} />
+                </section>
+                <section clasName="w-[30%]">
+                    {value[3]} <input type="radio" id="answer" name={props.id} value={value[3]} className="text-left" onChange={(e) => { saveAnswers(props.id, e) }} />
+                </section>
+
             </p>
-     
+
         </>
     )
 }
