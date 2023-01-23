6458776da5657fb9faefe7fe7da769574d214d0d
amani51 <amani.898919@gmail.com>
2023-01-23 11:56:32 +0200

    feat:add users-admin panel

--- app/admin/quizzes/UpdateQus.js
+++ app/admin/quizzes/UpdateQus.js
@@ -3,7 +3,6 @@ import { Dialog, Transition } from "@headlessui/react";
 import { AuthContext } from "app/contexts/auth.js";
 import axios from "axios";
 
-
 export default function UpdateQus({ isOpen, close, quizInfo, setAllQuizzes }) {
   const { tokens, refresh } = useContext(AuthContext);
   const [open, setOpen] = useState(true);
@@ -17,23 +16,28 @@ export default function UpdateQus({ isOpen, close, quizInfo, setAllQuizzes }) {
       Authorization: `Bearer ${access}`,
     },
   };
-  
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const url = `https://dhiker-api-v1.herokuapp.com/api/v1/quiz/`;
-    const correct=e.target.correct.value;
-    const wrong1=e.target.wrong1.value;
-    const wrong2=e.target.wrong2.value;
-    const wrong3=e.target.wrong3.value;
+    const correct = e.target.correct.value;
+    const wrong1 = e.target.wrong1.value;
+    const wrong2 = e.target.wrong2.value;
+    const wrong3 = e.target.wrong3.value;
     const obj = {
       owner: localStorage.getItem("userId"),
       question: e.target.question.value,
-      choices:JSON.stringify({'correct':correct,'wrong1':wrong1,'wrong2':wrong2,'wrong3':wrong3}),
+      choices: JSON.stringify({
+        correct: correct,
+        wrong1: wrong1,
+        wrong2: wrong2,
+        wrong3: wrong3,
+      }),
       type: quizInfo.type,
-      id:quizInfo.id
+      id: quizInfo.id,
     };
     axios
-    .put(`${url}${quizInfo.id}`, obj, config)
+      .put(`${url}${quizInfo.id}`, obj, config)
       .then((result) => {
         console.log(result.data);
         axios
@@ -56,7 +60,7 @@ export default function UpdateQus({ isOpen, close, quizInfo, setAllQuizzes }) {
       <Transition.Root show={isOpen} as={Fragment}>
         <Dialog
           as="div"
-          className="relative z-10"
+          className="relative z-10 bg-[#e5f2c4] "
           initialFocus={cancelButtonRef}
           onClose={setOpen}
         >
@@ -69,10 +73,10 @@ export default function UpdateQus({ isOpen, close, quizInfo, setAllQuizzes }) {
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            <div className="fixed inset-0 bg-[#e5f2c4]  bg-opacity-75 transition-opacity" />
           </Transition.Child>
 
-          <div className="fixed inset-0 z-10 overflow-y-auto text-xl">
+          <div className="fixed inset-0 z-10 overflow-y-auto text-xl ">
             <div className="flex min-h-full items-end justify-center text-center sm:items-center sm:p-0">
               <Transition.Child
                 as={Fragment}
@@ -83,15 +87,15 @@ export default function UpdateQus({ isOpen, close, quizInfo, setAllQuizzes }) {
                 leaveFrom="opacity-100 translate-y-0 "
                 leaveTo="opacity-0 translate-y-4 sm:translate-y-0 "
               >
-                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                  <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                    <div className="bg-white p-8  w-full border-solid shadow-2xl rounded-3xl">
-                      <button
-                        onClick={close}
-                        className="border-solid border-2 mb-16 border-gray-400 rounded float-left  hover:border-3 hover:border-gray-700"
-                      >
-                        ❌
-                      </button>
+                <Dialog.Panel className="relative transform overflow-hidden rounded-lg text-left bg-[#949e7b]  shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                  <div className="bg-[#949e7b]  px-4 pt-5 pb-4 sm:p-6 sm:pb-4 ">
+                    <button
+                      onClick={close}
+                      className="border-solid border-2 mb-16 border-gray-400 rounded float-left  hover:border-3 hover:border-gray-700"
+                    >
+                      ❌
+                    </button>
+                    <div className="bg-[#949e7b]  p-8  w-full border-solid  rounded-3xl">
                       <form action="" onSubmit={handleSubmit}>
                         <div className="flex items-center mb-5 ">
                           <input
@@ -100,13 +104,13 @@ export default function UpdateQus({ isOpen, close, quizInfo, setAllQuizzes }) {
                             name="question"
                             defaultValue={quizInfo.question}
                             required
-                            className="flex-1 py-2 border-b-2 border-gray-400 focus:border-green-400 
-                      text-gray-600
+                            className="flex-1 py-2 border-b-2 border-gray-400 focus:border-green-400 bg-[#e5f2c4] 
+                      text-[#252f0b]
                       outline-none text-right"
                           />
                           <label
                             className="inline-block w-20 mr-6 text-right 
-                                 font-bold text-gray-600 w-1/2"
+                                 font-bold text-[#252f0b] w-1/2"
                           >
                             السؤال
                           </label>
@@ -117,15 +121,21 @@ export default function UpdateQus({ isOpen, close, quizInfo, setAllQuizzes }) {
                             type="text"
                             id="correct"
                             name="correct"
-                            defaultValue= {typeof(quizInfo.choices)=="string" ?JSON.parse(quizInfo.choices)['correct']:typeof(quizInfo.choices)=='object'? quizInfo.choices['correct']:"صحيح"}
-                            
+                            defaultValue={
+                              typeof quizInfo.choices == "string"
+                                ? JSON.parse(quizInfo.choices)["correct"]
+                                : typeof quizInfo.choices == "object"
+                                ? quizInfo.choices["correct"]
+                                : "صحيح"
+                            }
                             required
-                            className="flex-1 py-2 border-b-2 border-gray-400  
-                      text-gray-600 text-right "
+                            className="flex-1 py-2 border-b-2 border-gray-400 focus:border-green-400 bg-[#e5f2c4] 
+                      text-[#252f0b]
+                      outline-none text-right"
                           />
                           <label
-                            className="inline-block w-20 mr-6 text-right
-                                    font-bold text-gray-600 w-1/2"
+                            className="inline-block w-20 mr-6 text-right 
+                                 font-bold text-[#252f0b] w-1/2"
                           >
                             الإجابة الصحيحة
                           </label>
@@ -135,15 +145,21 @@ export default function UpdateQus({ isOpen, close, quizInfo, setAllQuizzes }) {
                             type="text"
                             id="wrong1"
                             name="wrong1"
-                            defaultValue= {typeof(quizInfo.choices)=="string" ?JSON.parse(quizInfo.choices)['wrong1']:typeof(quizInfo.choices)=='object'? quizInfo.choices['wrong1']:"خطأ1"}
-
+                            defaultValue={
+                              typeof quizInfo.choices == "string"
+                                ? JSON.parse(quizInfo.choices)["wrong1"]
+                                : typeof quizInfo.choices == "object"
+                                ? quizInfo.choices["wrong1"]
+                                : "خطأ1"
+                            }
                             required
-                            className="flex-1 py-2 border-b-2 border-gray-400  
-                      text-gray-600 text-right "
+                            className="flex-1 py-2 border-b-2 border-gray-400 focus:border-green-400 bg-[#e5f2c4] 
+                      text-[#252f0b]
+                      outline-none text-right"
                           />
                           <label
-                            className="inline-block w-20 mr-6 text-right
-                                    font-bold text-gray-600 w-1/2"
+                            className="inline-block w-20 mr-6 text-right 
+                                 font-bold text-[#252f0b] w-1/2"
                           >
                             الخيار الخاطئ الأول
                           </label>
@@ -154,14 +170,20 @@ export default function UpdateQus({ isOpen, close, quizInfo, setAllQuizzes }) {
                             id="wrong2"
                             name="wrong2"
                             required
-                            defaultValue= {typeof(quizInfo.choices)=="string" ?JSON.parse(quizInfo.choices)['wrong2']:typeof(quizInfo.choices)=='object'? quizInfo.choices['wrong2']:"خطأ2"}
-
-                            className="flex-1 py-2 border-b-2 border-gray-400  
-                      text-gray-600 text-right "
+                            defaultValue={
+                              typeof quizInfo.choices == "string"
+                                ? JSON.parse(quizInfo.choices)["wrong2"]
+                                : typeof quizInfo.choices == "object"
+                                ? quizInfo.choices["wrong2"]
+                                : "خطأ2"
+                            }
+                            className="flex-1 py-2 border-b-2 border-gray-400 focus:border-green-400 bg-[#e5f2c4] 
+                      text-[#252f0b]
+                      outline-none text-right"
                           />
                           <label
-                            className="inline-block w-20 mr-6 text-right
-                                    font-bold text-gray-600 w-1/2"
+                            className="inline-block w-20 mr-6 text-right 
+                                 font-bold text-[#252f0b] w-1/2"
                           >
                             الخيار الخاطئ الثاني
                           </label>
@@ -172,20 +194,26 @@ export default function UpdateQus({ isOpen, close, quizInfo, setAllQuizzes }) {
                             id="wrong3"
                             name="wrong3"
                             required
-                            defaultValue= {typeof(quizInfo.choices)=="string" ?JSON.parse(quizInfo.choices)['wrong3']:typeof(quizInfo.choices)=='object'? quizInfo.choices['wrong3']:"خطأ3"}
-
-                            className="flex-1 py-2 border-b-2 border-gray-400  
-                      text-gray-600 text-right "
+                            defaultValue={
+                              typeof quizInfo.choices == "string"
+                                ? JSON.parse(quizInfo.choices)["wrong3"]
+                                : typeof quizInfo.choices == "object"
+                                ? quizInfo.choices["wrong3"]
+                                : "خطأ3"
+                            }
+                            className="flex-1 py-2 border-b-2 border-gray-400 focus:border-green-400 bg-[#e5f2c4] 
+                      text-[#252f0b]
+                      outline-none text-right"
                           />
                           <label
-                            className="inline-block w-20 mr-6 text-right
-                                    font-bold text-gray-600 w-1/2"
+                            className="inline-block w-20 mr-6 text-right 
+                                 font-bold text-[#252f0b] w-1/2"
                           >
                             الخيار الخاطئ الثالث
                           </label>
                         </div>
                         <div className="text-left">
-                          <button className="py-3 px-8 bg-teal-600 text-white font-bold rounded-3xl hover:bg-teal-800 shadow-2xl">
+                          <button className="py-3 px-8 bg-teal-800 text-white font-bold rounded-3xl hover:bg-teal-600 shadow-2xl">
                             تعديل
                           </button>
                         </div>
--- app/admin/users/page.js
+++ app/admin/users/page.js
@@ -1,10 +1,170 @@
-import Link from "next/link"
+"use client";
+import Link from "next/link";
+import { useState, useContext, useEffect } from "react";
+import { AuthContext } from "app/contexts/auth.js";
+import axios from "axios";
 
-export default function Users(){
-    return(
-        <>
-        <h1>test users panel </h1>
-        <h1><Link href='/admin'> home </Link></h1>
-        </>
-    ) 
+export default function Users() {
+  const { tokens, refresh } = useContext(AuthContext);
+  const refresh_string = localStorage.getItem("refresh");
+  refresh(refresh_string);
+  const access = localStorage.getItem("access");
+  const config = {
+    headers: {
+      Authorization: `Bearer ${access}`,
+    },
+  };
+
+  const [female,setFemale]=useState(0)
+  const [male,setMale]=useState(0)
+  const [admin,setAdmin]=useState(0)
+  const [percentMale,setPercentMale]=useState(0)
+  const [percentFemale,setPercentFemale]=useState(0)
+
+  const femaleMale=(items)=>{
+    let female=0,male=0,admin=0;
+    let x= items.map((item)=>{
+     if (item['gender']=="female"){
+      female++
+     }
+     else if (item['gender']=="male"){
+      male++
+     }
+     else{
+      admin++
+     }
+    return [female,male]
+    })
+    setFemale(female)
+    setMale(male)
+    setAdmin(admin)
+    
+    
+  }
+
+
+  const [users, setUsers] = useState([]);
+  const url = `https://dhiker-api-v1.herokuapp.com/api/accounts/users/`;
+  useEffect(() => {
+    axios
+      .get(url, config)
+      .then((result) => {
+        setUsers(result.data);
+        console.log(result.data);
+        femaleMale(result.data)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <div className="py-10 bg-[#e5f2c4]">
+      <Link
+        href="/admin"
+        className="flex w-[14%] px-8 py-2 mb-4 ml-8 text-lg text-black shadow-2xl rounded-3xl hover:text-[#e5f2c4] hover:bg-[#3a451c] shadow-gray-500 text-center bg-[#949e7b] "
+      >
+        {" "}
+        عودة لصفحة الرئيسية
+      </Link>
+      <div class="w-full relative flex ct-docs-disable-sidebar-content overflow-x-hidden items-center">
+        <div class="relative bg-blueGray-100 w-full">
+ 
+          <div class="relative pt-16 pb-32 bg-lightBlue-500 w-full">
+            <div class="px-4 md:px-6 mx-auto w-full">
+              <div>
+                <div class="flex flex-wrap">
+                  <div class="w-full lg:w-6/12 xl:w-3/12 px-4">
+                    <div class="relative flex flex-col min-w-0 break-words bg-white rounded-lg mb-6 xl:mb-0 shadow-lg">
+                      <div class="flex-auto px-4 bg-[#949e7b] rounded-xl">
+                        <div class="flex flex-wrap mt-4">
+                          <div class="relative w-auto pl-4 mt-4 flex-initial">
+                            <div class="text-white px-3 my-3 text-center inline-flex items-center justify-center w-16 h-16 shadow-lg rounded-full bg-red-500">
+                              <i class="far fa-chart-bar"></i>
+                             {(female / users.length) * 100}% 
+                            </div>
+                          </div>
+                          <div class="relative w-full pr-4 max-w-full flex-grow flex-1 text-right ">
+                            <h5 class="text-[#252f0b] uppercase font-bold text-2xl border-b-2 border-[#252f0b] mb-2">
+                              الإناث
+                            </h5>
+                            <span class="font-bold text-xl  text-[#252f0b]  text-[#252f0b]">{female}</span>
+                          </div>
+                          
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="w-full lg:w-6/12 xl:w-3/12 px-4">
+                    <div class="relative flex flex-col min-w-0 break-words bg-white rounded-lg mb-6 xl:mb-0 shadow-lg">
+                      <div class="flex-auto px-4 bg-[#949e7b] rounded-xl">
+                        <div class="flex flex-wrap mt-4">
+                          <div class="relative w-auto pl-4 mt-4 flex-initial">
+                            <div class="text-white px-3 my-3 text-center inline-flex items-center justify-center w-16 h-16 shadow-lg rounded-full bg-blue-600">
+                              <i class="far fa-chart-bar"></i>
+                            {(male / users.length) * 100}% 
+                            </div>
+                          </div>
+                          <div class="relative w-full pr-4 max-w-full flex-grow flex-1 text-right ">
+                            <h5 class="text-[#252f0b] uppercase font-bold text-2xl border-b-2 border-[#252f0b] mb-2">
+                              الذكور
+                            </h5>
+                            <span class="font-bold text-xl  text-[#252f0b] ">{male}</span>
+                          </div>
+                          
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="w-full lg:w-6/12 xl:w-3/12 px-4">
+                    <div class="relative flex flex-col min-w-0 break-words bg-white rounded-lg mb-6 xl:mb-0 shadow-lg">
+                      <div class="flex-auto px-4 bg-[#949e7b] rounded-xl">
+                        <div class="flex flex-wrap mt-4">
+                          <div class="relative w-auto pl-4 mt-4 flex-initial">
+                            <div class="text-white px-3 my-3 text-center inline-flex items-center justify-center w-16 h-16 shadow-lg rounded-full bg-[#3a451c]">
+                              <i class="far fa-chart-bar"></i>
+                             {(admin / users.length) * 100}% 
+                            </div>
+                          </div>
+                          <div class="relative w-full pr-4 max-w-full flex-grow flex-1 text-right ">
+                            <h5 class="text-[#252f0b] uppercase font-bold text-2xl border-b-2 border-[#252f0b] mb-2">
+                              المسؤولون
+                            </h5>
+                            <span class="font-bold text-xl  text-[#252f0b] ">{admin}</span>
+                          </div>
+                          
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="w-full lg:w-6/12 xl:w-3/12 px-4">
+                    <div class="relative flex flex-col min-w-0 break-words bg-white rounded-lg mb-6 xl:mb-0 shadow-lg">
+                      <div class="flex-auto px-4 bg-[#949e7b] rounded-xl">
+                        <div class="flex flex-wrap mt-4">
+                          <div class="relative w-auto pl-4 mt-4 flex-initial">
+                            <div class="text-white px-3 my-3 text-center inline-flex items-center justify-center w-16 h-16 shadow-lg rounded-full bg-[#252f0b]">
+                              <i class="far fa-chart-bar"></i>
+                              100%
+                            </div>
+                          </div>
+                          <div class="relative w-full pr-4 max-w-full flex-grow flex-1 text-right  ">
+                            <h5 class="text-[#252f0b] uppercase font-bold text-3xl border-b-2 border-[#252f0b] mb-2">
+                              المستخدمون
+                            </h5>
+                            <span class="font-bold text-xl text-[#252f0b] ">{users.length}</span>
+                          </div>
+                          
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
